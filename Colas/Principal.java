70879f71c826385706608ed0748bad69fb7ec68d
Sala K 402 <salak402@salasfr.itm.edu.co>
2025-03-27 07:24:08 -0500

    sergio

---
+++ Colas/Principal.java
@@ -0,0 +1,39 @@
+package Colas;
+
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean bandera = true;
+        int opt=0;
+        opt = sc.nextInt();
+        String menu = "Bienvenidos a Frater!+\n"
+        +"Selecciona una opcion del menu\n"
+        +"1. Hamburguesa\n"
+        +"2. Perro caliente\n"
+        +"3. Salchipapas\n"
+        +"4. Salir";
+
+        while (bandera) {
+            switch (opt) {
+                case 1:
+                    
+                    break;
+                
+            
+                default:
+                    break;
+            }
+    
+        }
+
+        
+
+        
+
+
+    }
+    
+}
